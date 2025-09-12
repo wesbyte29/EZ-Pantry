ab2597f8b0c3c0c8943d9836adf146ebf00b11cc
Wesley Fisher <wesley@fisherusa.org>
2025-09-12 15:42:15 -0500

    three new pages

---
+++ lib/recipes_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecipesPage extends StatelessWidget {
+  const RecipesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'This is the Recipes Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
---
+++ lib/shopping_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoppingPage extends StatelessWidget {
+  const ShoppingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'This is the Shopping Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
